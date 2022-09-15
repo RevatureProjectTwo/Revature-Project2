50af987cfa6dbab1415c6a06df39ba12cb68e1b4
Wesley P <wes.j.pruett@gmail.com>
2022-09-15 13:46:00 -0600

    First Push to Github9

---
+++ project2-WesP-SamvelA/src/main/java/com/Revature/ecommerce/project2WesPSamvelA/Project2WesPSamvelAApplication.java
@@ -0,0 +1,13 @@
+package com.Revature.ecommerce.project2WesPSamvelA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2WesPSamvelAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2WesPSamvelAApplication.class, args);
+	}
+
+}
