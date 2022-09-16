060f40457decd6363b3ece703a3955e022c8dbf6
Wesley P <wes.j.pruett@gmail.com>
2022-09-16 10:28:25 -0600

    added entity files and application properties

--- project2-WesP-SamvelA/src/main/java/com/Revature/ecommerce/project2WesPSamvelA/entity/UserEntity.java
+++ project2-WesP-SamvelA/src/main/java/com/Revature/ecommerce/project2WesPSamvelA/entity/UserEntity.java
@@ -1,6 +1,10 @@
 package com.Revature.ecommerce.project2WesPSamvelA.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -15,8 +19,35 @@ import lombok.ToString;
 @Setter
 @ToString
 @Entity
-@Table(????)
+@Table(name="user_details")
 public class UserEntity 
 {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name="user_id")
+	private int userId;
+	
+	@Column(name="user_username")
+	private String userUserName;
+	
+	@Column(name="user_password")
+	private String userPassword;
+	
+	@Column(name="user_first_name")
+	private String userFirstName;
+	
+	@Column(name="user_last_name")
+	private String userLastName;
+	
+	@Column(name="user_address")
+	private String userAddress;
+	
+	@Column(name="user_email")
+	private String userEmail;
+	
+	@Column(name="user_contact_number")
+	private String userContact;
+	
+	@Column(name="user_is_employee")
+	private boolean userEmployee;
 }
